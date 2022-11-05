d0bbae3565f295d1dd47312c604943faf90f131c
Christopher Huemmer <christopher.huemmer@me.com>
2022-11-05 17:22:06 +0100

    minor formatting

--- main.py
+++ main.py
@@ -28,71 +28,71 @@ def format_currency(value, currencySymbol):
     return f"{value}{currencySymbol}"
 
 
-def draw_portfolio(portfolio):
+def draw_header(gui):
+    name = Label(gui, text="Coin Name", bg="#142E54",
+                 fg="white", font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
+    name.grid(row=0, column=0, sticky=N+S+E+W)
+
+    name = Label(gui, text="Price", bg="#142E54", fg="white",
+                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
+    name.grid(row=0, column=1, sticky=N+S+E+W)
+
+    name = Label(gui, text="Coins Owned", bg="#142E54", fg="white",
+                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
+    name.grid(row=0, column=2, sticky=N+S+E+W)
+
+    name = Label(gui, text="Total Amount Paid", bg="#142E54", fg="white",
+                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
+    name.grid(row=0, column=3, sticky=N+S+E+W)
+
+    name = Label(gui, text="Current Value", bg="#142E54", fg="white",
+                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
+    name.grid(row=0, column=4, sticky=N+S+E+W)
+
+    name = Label(gui, text="P/L per Coin", bg="#142E54", fg="white",
+                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
+    name.grid(row=0, column=5, sticky=N+S+E+W)
+
+    name = Label(gui, text="Total P/L with coin", bg="#142E54", fg="white",
+                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
+    name.grid(row=0, column=6, sticky=N+S+E+W)
+
+
+def draw_portfolio(gui, portfolio):
     coin_row = 1
 
     for token, market in portfolio:
-        symbol = Label(pycrypto, text=market.symbol, bg="#F3F4F6", fg="black",
+        symbol = Label(gui, text=market.symbol, bg="#F3F4F6", fg="black",
                        font="Lato 12 bold", padx="2", pady="2", borderwidth=2, relief="groove")
         symbol.grid(row=coin_row, column=0, sticky=N+S+E+W)
 
-        current_price = Label(pycrypto, text=format_price(market.current_price),
+        current_price = Label(gui, text=format_price(market.current_price),
                               bg="#F3F4F6", fg="black", font="Lato 12 bold", padx="2", pady="2", borderwidth=2, relief="groove")
         current_price.grid(row=coin_row, column=1, sticky=N+S+E+W)
 
-        amount_owned = Label(pycrypto, text=token.amount_owned, bg="#F3F4F6", fg="black", font="Lato 12 bold",
+        amount_owned = Label(gui, text=token.amount_owned, bg="#F3F4F6", fg="black", font="Lato 12 bold",
                              padx="2", pady="2", borderwidth=2, relief="groove")
         amount_owned.grid(row=coin_row, column=2, sticky=N+S+E+W)
 
-        amount_paid = Label(pycrypto, text=format_currency(token.amount_paid, "€"),
+        amount_paid = Label(gui, text=format_currency(token.amount_paid, "€"),
                             bg="#F3F4F6", fg="black", font="Lato 12 bold", padx="2", pady="2", borderwidth=2, relief="groove")
         amount_paid.grid(row=coin_row, column=3, sticky=N+S+E+W)
 
-        current_value = Label(pycrypto, text=calculate_value(token.amount_owned, market.current_price), bg="#F3F4F6", fg="black", font="Lato 12 bold",
+        current_value = Label(gui, text=calculate_value(token.amount_owned, market.current_price), bg="#F3F4F6", fg="black", font="Lato 12 bold",
                               padx="2", pady="2", borderwidth=2, relief="groove")
         current_value.grid(row=coin_row, column=4, sticky=N+S+E+W)
 
-        profit_loss_per_coin = Label(pycrypto, text="0", bg="#F3F4F6", fg="black",
+        profit_loss_per_coin = Label(gui, text="0", bg="#F3F4F6", fg="black",
                                      font="Lato 12 bold", padx="2", pady="2", borderwidth=2, relief="groove")
         profit_loss_per_coin.grid(row=coin_row, column=5, sticky=N+S+E+W)
 
-        total_profit_loss_with_coin = Label(pycrypto, text="0", bg="#F3F4F6", fg="black",
+        total_profit_loss_with_coin = Label(gui, text="0", bg="#F3F4F6", fg="black",
                                             font="Lato 12 bold", padx="2", pady="2", borderwidth=2, relief="groove")
         total_profit_loss_with_coin.grid(
             row=coin_row, column=6, sticky=N+S+E+W)
         coin_row = coin_row + 1
 
 
-def draw_header(pycrypto):
-    name = Label(pycrypto, text="Coin Name", bg="#142E54",
-                 fg="white", font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
-    name.grid(row=0, column=0, sticky=N+S+E+W)
-
-    name = Label(pycrypto, text="Price", bg="#142E54", fg="white",
-                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
-    name.grid(row=0, column=1, sticky=N+S+E+W)
-
-    name = Label(pycrypto, text="Coins Owned", bg="#142E54", fg="white",
-                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
-    name.grid(row=0, column=2, sticky=N+S+E+W)
-
-    name = Label(pycrypto, text="Total Amount Paid", bg="#142E54", fg="white",
-                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
-    name.grid(row=0, column=3, sticky=N+S+E+W)
-
-    name = Label(pycrypto, text="Current Value", bg="#142E54", fg="white",
-                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
-    name.grid(row=0, column=4, sticky=N+S+E+W)
-
-    name = Label(pycrypto, text="P/L per Coin", bg="#142E54", fg="white",
-                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
-    name.grid(row=0, column=5, sticky=N+S+E+W)
-
-    name = Label(pycrypto, text="Total P/L with coin", bg="#142E54", fg="white",
-                 font="Lato 12 bold", padx="5", pady="5", borderwidth=2, relief="groove")
-    name.grid(row=0, column=6, sticky=N+S+E+W)
-
-
 draw_header(pycrypto)
-draw_portfolio(viewModel)
+draw_portfolio(pycrypto, viewModel)
 pycrypto.mainloop()
